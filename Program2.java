1c99000609349e984f354457848c5525a2f0cbd1
~ Ananya ~ <playing230106@gmail.com>
2025-08-05 01:00:12 +0530

    Create Program2.java

---
+++ Program2.java
@@ -0,0 +1,9 @@
+//WAP to print your address i) using single print ii) using multiple println
+class Main {
+    public static void main(String[] args) {
+        System.out.print("C-803, Tower-18,\nRajnagar extension,\nGhaziabad\n");
+        System.out.println("C-803, Tower-18");
+        System.out.println("Rajnagar extension");
+        System.out.println("Ghaziabad");
+    }
+}
